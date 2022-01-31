4e8e95100b9198a255b7d0e6e5679f678ac9eedf
Er4ikk <g.zemignani@studenti.unimol.it>
2022-01-31 15:18:30 +0100

    initializing-test-cases-and-fix-utf-8-mavn-warning

---
+++ fruit-shop/src/test/java/app/CataogueTest.java
@@ -0,0 +1,72 @@
+package app;
+
+import org.junit.Test;
+
+public class CataogueTest {
+
+    @Test
+    public void testCatalogue() {
+
+    }
+
+    @Test
+    public void testSetProductList() {
+
+    }
+
+    @Test
+    public void testGetProductList() {
+
+    }
+
+    @Test
+    public void testInsertProduct() {
+
+    }
+
+    @Test
+    public void testDeleteProduct() {
+
+    }
+
+    @Test
+    public void testSearch() {
+
+    }
+
+    @Test
+    public void testSellProduct() {
+
+    }
+
+    @Test
+    public void testGetMoney_earned() {
+
+    }
+
+    @Test
+    public void testSetMoney_earned() {
+
+    }
+
+    @Test
+    public void testSetProduct() {
+
+    }
+
+    @Test
+    public void testGetProduct() {
+
+    }
+
+    @Test
+    public void testGetIva() {
+
+    }
+
+    @Test
+    public void testSetIva() {
+
+    }
+
+}
---
+++ fruit-shop/src/test/java/app/ProductTest.java
@@ -0,0 +1,60 @@
+package app;
+
+import org.junit.Test;
+
+public class ProductTest {
+
+    @Test
+    public void testProduct() {
+    }
+
+    @Test
+    public void testSetProductId() {
+
+    }
+
+    @Test
+    public void testGetProductId() {
+
+    }
+
+    @Test
+    public void testSetName() {
+
+    }
+
+    @Test
+    public void testGetName() {
+    }
+
+    @Test
+    public void testSetQuantity() {
+
+    }
+
+    @Test
+    public void testGetQuantity() {
+
+    }
+
+    @Test
+    public void testSetPrice() {
+
+    }
+
+    @Test
+    public void testGetPrice() {
+
+    }
+
+    @Test
+    public void testSetType() {
+
+    }
+
+    @Test
+    public void testGetType() {
+
+    }
+
+}
