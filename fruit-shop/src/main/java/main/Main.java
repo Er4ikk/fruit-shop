c07d6338eda55c39579b469c4cdb83506fb59fe6
Er4ikk <g.zemignani@studenti.unimol.it>
2022-01-31 09:48:12 +0100

    added-mainLoppUi-to-main-fixed-pom.xml

--- fruit-shop/src/main/java/main/Main.java
+++ fruit-shop/src/main/java/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import ui.FruitShopUi;
+
 public class Main {
 
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
-	System.out.println("Hello ");
+	FruitShopUi shopUi = new FruitShopUi();
+	shopUi.mainLoopUi();
     }
 
 }
