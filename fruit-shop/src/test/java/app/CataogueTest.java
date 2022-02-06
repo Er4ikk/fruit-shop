d2e8d9f673e172c5902e65333b2a6c49c2f286a4
Er4ikk <g.zemignani@studenti.unimol.it>
2022-02-06 17:29:31 +0100

    some tests

--- fruit-shop/src/test/java/app/CataogueTest.java
+++ fruit-shop/src/test/java/app/CataogueTest.java
@@ -24,52 +24,52 @@ public class CataogueTest {
 
     @Test
     public void testInsertProduct() {
-
+	this.testCatalaogue.insertProduct(new Product());
     }
 
     @Test
     public void testDeleteProduct() {
-
+	this.testCatalaogue.deleteProduct(0);
     }
 
     @Test
     public void testSearch() {
-
+	this.testCatalaogue.search(0);
     }
 
     @Test
     public void testSellProduct() {
-
+	this.testCatalaogue.sellProduct(0);
     }
 
     @Test
     public void testGetMoney_earned() {
-
+	this.testCatalaogue.getMoney_earned();
     }
 
     @Test
     public void testSetMoney_earned() {
-
+	this.testCatalaogue.setMoney_earned(0);
     }
 
     @Test
     public void testSetProduct() {
-
+	this.testCatalaogue.setProduct(new Product());
     }
 
     @Test
     public void testGetProduct() {
-
+	this.testCatalaogue.getProduct();
     }
 
     @Test
     public void testGetIva() {
-
+	this.testCatalaogue.getIva();
     }
 
     @Test
     public void testSetIva() {
-
+	this.testCatalaogue.setIva(0);
     }
 
 }
